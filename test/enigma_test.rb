0c52fef22f84a260cb2f0b17b4b8a6744cfbaaa8
Joseph Budina <joebud3@gmail.com>
2021-01-14 16:24:06 -0700

    Add decrypt test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -11,4 +11,24 @@ class EnigmaTest < Minitest::Test
   def test_it_exists
     assert_instance_of Enigma, @enigma
   end
+
+  def test_it_encrypts
+    expected = {
+          encryption: "keder ohulw",
+          key: "02715",
+          date: "040895"
+        }
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_it_decrypts
+    expected = {
+      encryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
 end
