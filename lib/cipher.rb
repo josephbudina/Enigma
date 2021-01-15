0179bd9166c07547a9b0fd70519139c1add6a078
Joseph Budina <joebud3@gmail.com>
2021-01-15 12:15:11 -0700

    initialize cypher

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,4 +1,7 @@
 class Cipher
+  attr_reader :alphabet
+
   def initialize
+    @alphabet = ("a".."z").to_a << " "
   end
 end
--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -9,5 +9,8 @@ class CipherTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Cipher, @cipher
+
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal expected, @cipher.alphabet
   end
 end
