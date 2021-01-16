1fef171238057922800ddb8cde1bf5c7aaa9da56
Joseph Budina <joebud3@gmail.com>
2021-01-16 13:37:55 -0700

    Add squared offsets and shift methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,7 +5,7 @@ class Enigma
     @alphabet = ("a".."z").to_a << " "
   end
 
-  def split_keys(key)
+  def split_key(key)
     split_keys = {
       a: key[0..1].to_i,
       b: key[1..2].to_i,
@@ -14,21 +14,30 @@ class Enigma
     }
   end
 
-  def encrypt(message, key, date)
-    a_key = key[0..1].to_i
-    b_key = key[1..2].to_i
-    c_key = key[2..3].to_i
-    d_key = key[3..4].to_i
+  def date_squared(date)
     date_squared = date.to_i ** 2
     date_squared_string = date_squared.to_s
-    offset_a = date_squared_string[-4].to_i
-    offset_b = date_squared_string[-3].to_i
-    offset_c = date_squared_string[-2].to_i
-    offset_d = date_squared_string[-1].to_i
-    a_shift = a_key + offset_a
-    b_shift = b_key + offset_b
-    c_shift = c_key + offset_c
-    d_shift = d_key + offset_d
+  end
+
+  def offsets(date)
+    offsets = {
+      a: date_squared(date)[-4].to_i,
+      b: date_squared(date)[-3].to_i,
+      c: date_squared(date)[-2].to_i,
+      d: date_squared(date)[-1].to_i
+    }
+  end
+
+  def shift(key, date)
+    shifts = {
+      a: split_key(key)[:a] + offsets(date)[:a],
+      b: split_key(key)[:b] + offsets(date)[:b],
+      c: split_key(key)[:c] + offsets(date)[:c],
+      d: split_key(key)[:d] + offsets(date)[:d],
+    }
+  end
+
+  def encrypt(message, key, date)
     # encrypted = Hash.new(0)
     # encrypted[:encryption] = message
     # encrypted[:key] = key
